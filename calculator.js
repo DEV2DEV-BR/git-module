5db28a7e874071847be2698a8ed4fce3b05c517e
Daniel <danieldeandradelopes@gmail.com>
2021-10-28 09:37:09 -0300

    add validations in calculator

--- calculator.js
+++ calculator.js
@@ -1,15 +1,27 @@
 function sum(a, b) {
+  if (!a || !b) {
+    return;
+  }
   return a + b;
 }
 
 function sub(a, b) {
+  if (!a || !b) {
+    return;
+  }
   return a - b;
 }
 
 function multiply(a, b) {
+  if (!a || !b) {
+    return;
+  }
   return a * b;
 }
 
 function division(a, b) {
+  if (!a || !b) {
+    return;
+  }
   return a / b;
 }
